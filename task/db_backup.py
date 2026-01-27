e416ed8ef3eb7c73a8d2ac3475b5c4d4405b1444
javohirtolipov44 <tolipovjavohir33@gmail.com>
2026-01-27 14:57:01 +0500

    Update db_backup.py

--- task/db_backup.py
+++ task/db_backup.py
@@ -28,9 +28,8 @@ async def backup_and_send(bot: Bot):
 
     # 📤 yuborish
     # ✅ Shu yerda open(..., "rb") bilan obyekt berish yetarli
-    with open(file_path, "rb") as f:
-        print(f)
-        await bot.send_document(chat_id=ADMINS[0], document=f)
+    
+    await bot.send_document(chat_id=ADMINS[0], document=f"../backups/{file_path}")
 
 
 async def scheduler(bot: Bot):
