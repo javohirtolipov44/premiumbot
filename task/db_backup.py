3d9d96b5382f33f2789a4c041840eb90434729e7
javohirtolipov44 <tolipovjavohir33@gmail.com>
2026-01-27 15:08:09 +0500

    Update db_backup.py

--- task/db_backup.py
+++ task/db_backup.py
@@ -10,7 +10,7 @@ DB_USER = "postgres"
 DB_PASSWORD = "123"
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BACKUP_DIR = os.path.join(BASE_DIR, "backups")
+BACKUP_DIR = os.path.join(BASE_DIR, "task")
 os.makedirs(BACKUP_DIR, exist_ok=True)
 
 BACKUP_INTERVAL = 60 * 60 * 24  # 24 soat
@@ -29,7 +29,7 @@ async def backup_and_send(bot: Bot):
     # 📤 yuborish
     # ✅ Shu yerda open(..., "rb") bilan obyekt berish yetarli
     
-    await bot.send_document(chat_id=ADMINS[0], document=f"../backups/db_backup_{today}.sql")
+    await bot.send_document(chat_id=ADMINS[0], document=f"db_backup_{today}.sql")
 
 
 async def scheduler(bot: Bot):
