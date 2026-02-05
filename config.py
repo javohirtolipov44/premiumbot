4e13ba93bc169f0d9cd5c33d907062e877028374
javohirtolipov44 <tolipovjavohir33@gmail.com>
2026-02-05 15:21:59 +0500

    Update PREMIUM_ID and PREMIUM_URL in config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 KARTA_NUMBER = 'https://t.me/anibro_premium/50'
-PREMIUM_ID = "-1002662208680"
-PREMIUM_URL = "https://t.me/+8bLISwZCt7EzMDky"
+PREMIUM_ID = "-1002141049390"
+PREMIUM_URL = "https://t.me/+v9ielwuqpys4M2Y6"
 INFO_URL = "https://t.me/anibro_premium/44"
 NOTIFY_DATA = 26
 SERVER_PRICE = 100000
